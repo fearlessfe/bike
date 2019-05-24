4cbcd3eb0da2bce7172ed7f0a045867e50ba6b9c
fearlessfe <505380967@qq.com>
2019-05-24 23:25:20 +0800

    menu

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Menu, Icon } from 'antd';
+import menuConfig from '../../resource/menuConfig';
+import './index.less';
+
+const { SubMenu } = Menu;
 
 export default class NavLeft extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuTreeNode: []
+    }
+  }
+
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(menuConfig);
+    this.setState({
+      menuTreeNode
+    })
+  }
+  // 菜单渲染
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return (
+        <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        NavLeft
+        <div className="logo">
+          <img src="" alt=""/>
+          <h1>Imooc MS</h1>
+        </div>
+        <Menu theme="dark">
+          {this.state.menuTreeNode}
+        </Menu>
       </div>
     )
   }
