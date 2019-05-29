b6ee1ac48a04f1993fa84cf4acd3d0ac383ea30d
fearlessfe <505380967@qq.com>
2019-05-29 23:38:47 +0800

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Row, Col } from 'antd';
+import Util from '../../utils/utils';
+import './index.less';
 
 export default class Header extends React.Component {
+  componentWillMount() {
+    this.setState({
+      uesrName: 'penny',
+      sysTime: ''
+    })
+    setInterval(() => {
+      let sysTime = Util.formatDate(new Date().getTime());
+      this.setState({
+        sysTime
+      })
+    }, 1000);
+  }
   render() {
     return (
-      <div>header</div>
+      <div className="header">
+        <Row className="header-top">
+          <Col>
+            <span>欢迎, {this.state.uesrName}</span>
+            <a href="">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span="4" className="breadcrumb-title">
+            首页
+          </Col>
+          <Col span="20" className="weather">
+            <span className="date">{this.state.sysTime}</span>
+            <span className="weather-detail">多云</span>
+          </Col>
+        </Row>
+      </div>
     );
   }
 } 
